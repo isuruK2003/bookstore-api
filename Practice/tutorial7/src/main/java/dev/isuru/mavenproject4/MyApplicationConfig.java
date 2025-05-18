7a4b89bc227d65009a5232b3dd572510bf343fe9
Isuru Karunaratna <93091809+isuruK2003@users.noreply.github.com>
2025-05-18 18:16:16 +0530

    refactor: remove redundant files

--- Practice/tutorial7/src/main/java/dev/isuru/mavenproject4/MyApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package dev.isuru.mavenproject4;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
-public class MyApplicationConfig extends ResourceConfig {
-    public MyApplicationConfig() {
-        register(HelloWorldResource.class);
-    }
-}
